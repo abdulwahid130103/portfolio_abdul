0a5bc1306cf2bfb1f977decc88241b3be0607ffe
abdulwahidsafiudin <yusrannganga@gmail.com>
2024-11-09 00:44:33 +0700

    create image popup and details about

--- src/js/index.js
+++ src/js/index.js
@@ -40,6 +40,76 @@ function scrollWindow() {
     });
   };
 }
+
+function zoomInImagePopupIcon() {
+  const icon = document.querySelector(".focus-icon-image-popup");
+  const imagePopup = document.querySelector(".img-popup");
+
+  icon.addEventListener("click", function () {
+    const scale = 1.5;
+
+    if (!imagePopup.classList.contains("zoomed")) {
+      imagePopup.classList.add("zoomed");
+      imagePopup.style.transformOrigin = "center";
+      imagePopup.style.transform = `scale(${scale})`;
+    } else {
+      imagePopup.classList.remove("zoomed");
+      imagePopup.style.transform = "scale(1)";
+      imagePopup.style.transformOrigin = "center";
+    }
+  });
+}
+
+function imageAboutmeClick() {
+  const aboutMeSection = document.getElementById("aboutme");
+  const imagePopupContainer = document.querySelector(".container-image-popup");
+  const popupImage = document.querySelector(".img-popup");
+  const body = document.querySelector("body");
+
+  aboutMeSection.addEventListener("click", (event) => {
+    const clickedImage = event.target;
+    if (clickedImage.tagName === "IMG") {
+      popupImage.src = clickedImage.src;
+      body.classList.remove("overflow-y-auto");
+      body.classList.add("overflow-y-hidden");
+      imagePopupContainer.classList.remove("hidden");
+    }
+  });
+
+  const deleteIcon = document.querySelector(".delete-icon-image-popup");
+
+  deleteIcon.addEventListener("click", () => {
+    imagePopupContainer.classList.add("hidden");
+    body.classList.remove("overflow-y-hidden");
+    body.classList.add("overflow-y-auto");
+    popupImage.src = "";
+  });
+}
+
+function zoomInImagePopup() {
+  const imagePopup = document.querySelector(".img-popup");
+
+  imagePopup.addEventListener("dblclick", function (e) {
+    const rect = imagePopup.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const scale = 2;
+    let translateX = ((x / rect.width) * 100 - 50) * -1;
+    let translateY = ((y / rect.height) * 100 - 50) * -1;
+
+    translateX = Math.max(-50, Math.min(translateX, 50));
+    translateY = Math.max(-50, Math.min(translateY, 50));
+
+    if (!imagePopup.classList.contains("zoomed")) {
+      imagePopup.classList.add("zoomed");
+      imagePopup.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%)`;
+    } else {
+      imagePopup.classList.remove("zoomed");
+      imagePopup.style.transform = "scale(1)";
+    }
+  });
+}
 $(document).ready(function () {
   $("#hamburger").click(function () {
     toggleNavbar();
@@ -48,5 +118,7 @@ $(document).ready(function () {
     toggleNavbar();
   });
   scrollWindow();
-  // scrollSmothMenu();
+  zoomInImagePopup();
+  zoomInImagePopupIcon();
+  imageAboutmeClick();
 });
